d9b8deeeb6d1e1b39d13d04f429eea0b0421197a
Zedrayami <minecraftpoon63@gmail.com>
2026-05-08 14:27:23 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -6,7 +6,7 @@
 
 const firebaseConfig = {
   apiKey:            "AIzaSyBI9o11kr2Heh8j8SAeeN5xdDlKVv1l8qA",
-  authDomain:        "idp-camp-2026.firebaseapp.com",
+  authDomain:        "zhermos.github.io",
   databaseURL:       "https://idp-camp-2026-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId:         "idp-camp-2026",
   storageBucket:     "idp-camp-2026.firebasestorage.app",
